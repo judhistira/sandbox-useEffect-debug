7c747115edb4e124c0f0703336a459a6898b7e1e
Judhistira Natha Junior <judhistira.natha@gmail.com>
2022-10-04 09:24:06 +0000

    Update 3 files

--- src/RandomUser.js
+++ src/RandomUser.js
@@ -8,21 +8,20 @@ const RandomUserTwo = () => {
   const [num, setNum] = useState(0);
   const [searchChange, setSearchChange] = useState('');
   const [searchWord, setSearchWord] = useState(
-    localStorage.setItem('user') || 'foobar'
+    localStorage.getItem('user') || 'foobar'
   );
   
   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    const fetchUser = () => {
-      const res = await fetch(`https://randomuser.me/api/?seed=${searchWord}`);
-      const data = await res.json();
+  useEffect(async () => {
+      const res =  await fetch(`https://randomuser.me/api/?seed=${searchWord}`);
+      const data =  res.json();
       setData(data.results);
-    };
-  }, []);
+    
+  }, [searchWord]);
 
   useEffect(() => {
-    localStorage.getItem('user', searchWord);
+    localStorage.setItem('user', searchWord);
   }, [searchWord]);
 
   useEffect(() => {
@@ -30,6 +29,10 @@ const RandomUserTwo = () => {
       console.log('i am running');
       setNum((prevNum) => (prevNum === 3 ? 0 : prevNum + 1));
     }, 7000);
+    
+    return () => {
+        clearInterval(colorInterval);
+    }
   }, []);
 
   return (
